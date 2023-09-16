80382f9e040bc392755948a0ba4d512ae0bd528b
umut7 <umut7@DESKTOP-3U4DR6O>
2023-09-16 17:29:47 +0200

    Add project files.

---
+++ Methods Exercise 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Methods_Exercise_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            string marda = "Marda";
+            string mali = "Mali";
+            string kozman = "Kozman";
+
+            Greetfriend(marda);
+            Greetfriend(mali);
+            Greetfriend(kozman);
+            Console.Read();
+        }
+        public static void Greetfriend(string name)
+        {
+            Console.WriteLine("Hi {0}, My friend", name);
+        }
+    }
+}
